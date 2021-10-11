9a2acd8c3fdf0a59698696d5c48abe656e6675df
ubaidmannethodi <ubaidmannethodi@gmail.com>
2021-10-11 19:26:07 +0530

    products added from api  and css file edited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { ProductsComponent } from './component/products/products.component';
 import { CartComponent } from './component/cart/cart.component';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { CartComponent } from './component/cart/cart.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/service/api-service.service.ts
+++ src/app/service/api-service.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiServiceService {
 
-  constructor() { }
+  constructor(private Http: HttpClient) { }
+  //Call datafrom api
+  getProducts() {
+    return this.Http.get<any>('https://fakestoreapi.com/products/').pipe(map((res) => {
+      return res;
+    }));
+  }
 }
