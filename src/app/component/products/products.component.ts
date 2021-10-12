dae97518c6f4d81cb61968f83f8e650d6f82bcda
ubaidmannethodi <ubaidmannethodi@gmail.com>
2021-10-12 19:35:48 +0530

    added some functions in service cart.service

--- src/app/component/products/products.component.ts
+++ src/app/component/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiServiceService } from 'src/app/service/api-service.service';
+import { CartService } from 'src/app/service/cart.service';
 
 @Component({
   selector: 'app-products',
@@ -8,14 +9,20 @@ import { ApiServiceService } from 'src/app/service/api-service.service';
 })
 export class ProductsComponent implements OnInit {
   public productList: any;
-  constructor(private api: ApiServiceService) { }
+  constructor(private api: ApiServiceService, private cartService: CartService) { }
 
   ngOnInit(): void {
     //Call api
     this.api.getProducts().subscribe((res) => {
       this.productList = res;
+      //assin  quantity and total to the object that not in the api data to add cart list
+      this.productList.forEach((a: any) => {
+        Object.assign(a, { quantity: 1, total: a.price });
+      });
 
     });
   }
-
+  addToCart(item: any) {
+    this.cartService.addToCart(item);
+  }
 }
